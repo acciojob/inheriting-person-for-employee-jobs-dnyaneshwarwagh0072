6b7f5a6d61af603b7714ccdeef13b99623d869cc
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-01-28 14:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,8 +24,3 @@ Employee.prototype.jobGreet = function () {
 window.Person = Person;
 window.Employee = Employee;
 
-const person = new Person("Alice", 25);
-person.greet();
-
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
