9e0c78f52f66ba5cf95f97c3389e628c546281bb
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-01-28 14:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-function Person(name, age) {
+class Person {
+	constructor(name, age){	
     this.name = name;
     this.age = age;
-}
-
-Person.prototype.greet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
+	 greet () {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
-
-function Employee(name, age, jobTitle) {
-    Person.call(this, name, age);
-    this.jobTitle = jobTitle;
 }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function () {
+class Employee extend Person {
+	constructor(name, age, jobTitle){	
+		super(name,age);
+	    this.jobTitle = jobTitle;
+	}
+	jobGreet () {
     console.log(
         `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
     );
 };
+}
 
 window.Person = Person;
 window.Employee = Employee;
